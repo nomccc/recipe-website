1541c384d94c231a7b5bfa80f03cb94f040a5d69
nomccc <nuravivah224@gmail.com>
2023-10-23 03:33:14 +0800

    add: ckeditor

--- src/pages/Admin/CreateRecipe.jsx
+++ src/pages/Admin/CreateRecipe.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Input from "../../components/Input/Input";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import './CKEditorStyle.module.css'
+import { CKEditor } from "@ckeditor/ckeditor5-react";
 
 const CreateRecipe = () => {
+
+  const config = {
+    blockToolbar: {
+      items: [ 'paragraph', 'heading1', 'heading2', '|', 'bulletedList', 'numberedList' ],
+      shouldNotGroupWhenFull: true
+    },
+    ckfinder: {
+      // Upload the images to the server using the CKFinder QuickUpload command.
+      uploadUrl:
+          // 'https://localhost:44374/api/v1/announcements/upload-file?command=QuickUpload&type=Images&responseType=json'
+          'https://ckeditor.com/apps/ckfinder/3.4.5/core/connector/php/connector.php?command=QuickUpload&type=Files&responseType=json'
+    },
+    mediaEmbed: {
+        previewsInData: true
+    },
+
+    table: {
+        contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+    }
+  };
   const classInput = " bg-white px-4 py-1 border-2 rounded-lg";
 
   return (
@@ -35,6 +58,26 @@ const CreateRecipe = () => {
               <Input name="porsiResep" text="Porsi" id="porsiResep" type="number" classname={classInput}/>
               <Input name="gambarResep" text="Gambar Resep" id="gambarResep" type="file" />
             </div>
+            <label htmlFor="bahanResep">Bahan-Bahan</label>
+            <CKEditor 
+style={'padding: 20px'}
+editor={ ClassicEditor }
+                    config={config}
+                    data=""
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
           </form>
         </div>
       </div>
